08168d26a3a49dbff8ffd521dc5fc472de1202b6
mingwei <huangmingwei88@qq.com>
2019-05-08 23:33:23 +0800

    【解决问题】: Initial Project 【修改内容】: 【提交人】： KEEP

---
+++ Docs/Questions.md
@@ -0,0 +1,14 @@
+# 问题记录
+
+## iOS报错 'config.h' file not found
+
+https://blog.csdn.net/qq_28978893/article/details/78720488
+
+```shell
+
+cd node_modules/react-native/third-party/glog-0.3.4
+../../scripts/ios-configure-glog.sh
+
+```
+
+
---
+++ iOSRNDemoProject/iOSRNDemoProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSRNDemoProject
+//
+//  Created by mingwei on 2019/5/8.
+//  Copyright © 2019年 fishmwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
